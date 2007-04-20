4bd37b3157c1eb0989150650e5703d0a49de6ea7
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-04-20 10:00:04 +0000

    checking for endpath

--- src/confdb/converter/PathWriterV1.java
+++ src/confdb/converter/PathWriterV1.java
@@ -8,7 +8,12 @@ public class PathWriterV1 implements IPathWriter {
 
 	public String toString( Path path, Converter converter, String indent ) 
 	{
-		String str = indent + "path " + path.name() + " = { ";
+		String str = indent;
+		if ( path.isEndPath() )
+			str += "endpath ";
+		else
+			str += "path ";
+		str += path.name() + " = { ";
 		for ( int i = 0; i < path.entryCount(); i++  )
 		{
 			str += path.entry(i).name();
