0635ddd282d68d825c167253858b5837eb60e5e0
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-05-15 15:20:52 +0000

    name and type of module handled correctly

--- src/confdb/converter/ModuleWriterV1.java
+++ src/confdb/converter/ModuleWriterV1.java
@@ -1,18 +1,44 @@
 package confdb.converter;
 
 import confdb.data.ModuleInstance;
+import confdb.data.Parameter;
 
-public class ModuleWriterV1  extends InstanceWriter implements IModuleWriter {
+public class ModuleWriterV1 implements IModuleWriter {
 	
-	protected Converter converter = null;
+	private IParameterWriter parameterWriter = null;
+	private Converter converter = null;
+	private static final String indent = "  ";
 
 	public String toString( ModuleInstance module ) 
 	{
-		return toString( "module", module, converter );
+		if ( parameterWriter == null )
+			parameterWriter = converter.getParameterWriter();
+		
+		String name = module.name();
+		String type = module.template().name();
+		
+		if (     module.template().instanceCount() == 1 
+		     &&  name.equals( type )  )
+			name = "";
+		
+		String str = indent + "module " +  name + " = " + type + " {";
+		if ( module.parameterCount() == 0 )
+			return str + "}" + converter.getNewline();
+			
+		str += converter.getNewline();
+		for ( int i = 0; i < module.parameterCount(); i++ )
+		{
+			Parameter parameter = module.parameter(i);
+			str += parameterWriter.toString( parameter, converter, indent + "  " );
+		}
+		str += indent + "}" + converter.getNewline();
+		return str;
 	}
+
 
 	public void setConverter(Converter converter) {
 		this.converter = converter;
 	}
 
+	
 }
