0a8c2f5715d1975d68dc88af6024358b8397d9c6
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-05-15 15:33:12 +0000

    name and type of module handled correctly

--- src/confdb/converter/html/HtmlModuleWriter.java
+++ src/confdb/converter/html/HtmlModuleWriter.java
@@ -2,19 +2,50 @@ package confdb.converter.html;
 
 import confdb.converter.Converter;
 import confdb.converter.IModuleWriter;
+import confdb.converter.IParameterWriter;
 import confdb.data.ModuleInstance;
+import confdb.data.Parameter;
 
-public class HtmlModuleWriter  extends HtmlInstanceWriter implements IModuleWriter {
+public class HtmlModuleWriter implements IModuleWriter {
 	
 	protected Converter converter = null;
+	private IParameterWriter parameterWriter = null;
+	
 
-	public String toString( ModuleInstance module ) 
+	public String toString( ModuleInstance instance ) 
 	{
-		return toString( "module", module, converter );
+		if ( parameterWriter == null )
+			parameterWriter = converter.getParameterWriter();
+
+		String name = instance.name();
+		String type = instance.template().name();
+		
+		if (     instance.template().instanceCount() == 1 
+		     &&  name.equals( type )  )
+			name = "";
+		
+		String str = "<tr><td>module</td>";
+		if ( name.length() != 0 )
+			str += "<td>" + name + "</td>";
+		str += "<td align=\"center\">=</td><td>" + type + "</td></tr>\n";
+		if ( instance.parameterCount() == 0 )
+			return str;
+			
+		str += "<tr><td></td><td colspan=\"5\"><table>";
+		for ( int i = 0; i < instance.parameterCount(); i++ )
+		{
+			Parameter parameter = instance.parameter(i);
+			str += parameterWriter.toString( parameter, converter, "  " );
+		}
+		str += "</table></td></tr>";
+		return str;
 	}
+
+
 
 	public void setConverter(Converter converter) {
 		this.converter = converter;
 	}
 
+	
 }
