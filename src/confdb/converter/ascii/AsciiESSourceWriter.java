47d748f98425bc541e8e41d56380a2ddb0b551bc
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-06-06 15:45:50 +0000

    service -> es_source

--- src/confdb/converter/ascii/AsciiESSourceWriter.java
+++ src/confdb/converter/ascii/AsciiESSourceWriter.java
@@ -2,14 +2,13 @@ package confdb.converter.ascii;
 
 import confdb.converter.Converter;
 import confdb.converter.IESSourceWriter;
-import confdb.converter.InstanceWriter;
 import confdb.data.ESSourceInstance;
 
-public class AsciiESSourceWriter extends InstanceWriter implements IESSourceWriter 
+public class AsciiESSourceWriter extends AsciiInstanceWriter implements IESSourceWriter 
 {
 	public String toString( ESSourceInstance essource, Converter converter ) 
 	{
-		return toString( "service", essource, converter );
+		return toString( "es_source", essource, converter );
 	}
 
 }
