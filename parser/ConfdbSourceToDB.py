6656ba0173c91431e42d9a11ad49212ac43b09ce
Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch>
2007-06-13 09:59:18 +0000

    Move es_modules back to their own table ("ESModuleTemplates").

--- parser/ConfdbSourceToDB.py
+++ parser/ConfdbSourceToDB.py
@@ -439,7 +439,7 @@ class ConfdbSourceToDB:
 
 					    sealcomponenttuple.append(sealessource)
 
-					    self.ScanComponent(sealclass, packagedir,package+"/"+subdir,source_tree,1)
+					    self.ScanComponent(sealclass, packagedir,package+"/"+subdir,source_tree,5)
 
 					    foundcomponent = 1			
 
