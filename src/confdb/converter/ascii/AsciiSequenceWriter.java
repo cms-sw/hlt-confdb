6b55e9c4d389386935a8179edf39486f0ec09c7c
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-08-23 12:51:32 +0000

    decorate(), decorateName()  added

--- src/confdb/converter/ascii/AsciiSequenceWriter.java
+++ src/confdb/converter/ascii/AsciiSequenceWriter.java
@@ -10,15 +10,26 @@ public class AsciiSequenceWriter implements ISequenceWriter {
 
 	public String toString( Sequence sequence, Converter converter ) 
 	{
-		String str = indent + "sequence " + sequence.name() + " = { ";
+		String str = indent + "sequence " + decorateName( sequence.name()) 
+			       + " = { ";
 		for ( int i = 0; i < sequence.entryCount(); i++  )
 		{
-			str += sequence.entry(i).name();
+			str += decorate( sequence.entry(i).name() );
 			if ( i + 1 < sequence.entryCount() )
 				str += " & ";
 		}
 		str += " }" + converter.getNewline();
 		return str;
+	}
+	
+	protected String decorateName( String name )
+	{
+		return name;
+	}
+
+	protected String decorate( String name )
+	{
+		return name;
 	}
 
 }
