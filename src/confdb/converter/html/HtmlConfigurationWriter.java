1a25f8d00b15fe1daa52f4e79086c963815b7803
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-08-23 14:11:22 +0000

    html tuning

--- src/confdb/converter/html/HtmlConfigurationWriter.java
+++ src/confdb/converter/html/HtmlConfigurationWriter.java
@@ -29,41 +29,51 @@ public class HtmlConfigurationWriter implements IConfigurationWriter
 	{
 		String str = "process <b>" + conf.processName() + "</b> = {" + converter.getNewline();
 
-		str += converter.getNewline();
-		
+		str += converter.getNewline(); 
+
+		str += "<a name=\"psets\"></a>"; 
 		IParameterWriter parameterWriter = converter.getParameterWriter();
 		for ( int i = 0; i < conf.psetCount(); i++ )
 		{
 			Parameter pset = conf.pset(i);
 			str += parameterWriter.toString( pset, converter, "  " );
 		}
+		if ( conf.psetCount() > 0 )
+			str += converter.getNewline(); 
 
-
+		str += "<a name=\"paths\"></a>"; 
 		IPathWriter pathWriter = converter.getPathWriter();
 		for ( int i = 0; i < conf.pathCount(); i++ )
 		{
 			Path path = conf.path(i);
 			str += pathWriter.toString( path, converter, "  " );
 		}
+		if ( conf.pathCount() > 0 )
+			str += converter.getNewline(); 
 
-		str += converter.getNewline();
 		
+		str += "<a name=\"sequences\"></a>"; 
 		ISequenceWriter sequenceWriter = converter.getSequenceWriter();
 		for ( int i = 0; i < conf.sequenceCount(); i++ )
 		{
 			Sequence sequence = conf.sequence(i);
 			str += sequenceWriter.toString(sequence, converter );
 		}
+		if ( conf.sequenceCount() > 0 )
+			str += converter.getNewline(); 
 
-		str += converter.getNewline();
 		
+		str += "<a name=\"modules\"></a>"; 
 		IModuleWriter moduleWriter = converter.getModuleWriter();
 		for ( int i = 0; i < conf.moduleCount(); i++ )
 		{
 			ModuleInstance module = conf.module(i);
 			str += moduleWriter.toString( module );
 		}
+		if ( conf.moduleCount() > 0 )
+			str += converter.getNewline(); 
 
+		str += "<a name=\"edsources\"></a>"; 
 		IEDSourceWriter edsourceWriter = converter.getEDSourceWriter();
 		for ( int i = 0; i < conf.edsourceCount(); i++ )
 		{
@@ -72,23 +82,32 @@ public class HtmlConfigurationWriter implements IConfigurationWriter
 		}
 		if ( conf.edsourceCount() == 0 )  // edsource may be overridden
 			str += edsourceWriter.toString( null, converter );
+		if ( conf.edsourceCount() > 0 )
+			str += converter.getNewline(); 
 
+
+		str += "<a name=\"essources\"></a>"; 
 		IESSourceWriter essourceWriter = converter.getESSourceWriter();
 		for ( int i = 0; i < conf.essourceCount(); i++ )
 		{
 			ESSourceInstance essource = conf.essource(i);
 			str += essourceWriter.toString(essource, converter);
 		}
+		if ( conf.essourceCount() > 0 )
+			str += converter.getNewline(); 
 
 
+		str += "<a name=\"esmodules\"></a>"; 
 		IESModuleWriter esmoduleWriter = converter.getESModuleWriter();
 		for ( int i = 0; i < conf.esmoduleCount(); i++ )
 		{
 			ESModuleInstance esmodule = conf.esmodule(i);
 			str += esmoduleWriter.toString(esmodule,converter);
 		}
+		if ( conf.esmoduleCount() > 0 )
+			str += converter.getNewline(); 
 
-
+		str += "<a name=\"services\"></a>"; 
 		IServiceWriter serviceWriter = converter.getServiceWriter();
 		for ( int i = 0; i < conf.serviceCount(); i++ )
 		{
