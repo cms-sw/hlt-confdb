15494fbad1af467e628e6978ac39c4cbcc57ad91
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-10-24 13:45:15 +0000

    using StringBuffer

--- src/confdb/converter/ascii/AsciiInstanceWriter.java
+++ src/confdb/converter/ascii/AsciiInstanceWriter.java
@@ -12,33 +12,33 @@ public class AsciiInstanceWriter {
 	private IParameterWriter parameterWriter = null;
 	
 	protected String toString(String type,Instance instance,ConverterEngine converterEngine)
-        {
+	{
 		if ( parameterWriter == null )
 			parameterWriter = converterEngine.getParameterWriter();
 		
 		boolean needInstanceLabel = !(type.equals("source")||
 					      type.equals("service"));
 
-		String str = (needInstanceLabel)
+		StringBuffer str = new StringBuffer( (needInstanceLabel)
 		    ?
 		    indent + type + " " +
 		    instance.name() + " = " +
 		    instance.template().name() + " {"
 		    :
-		    indent + type + " = " + instance.template().name() + " {";
+		    indent + type + " = " + instance.template().name() + " {" );
 
 		
 		if ( instance.parameterCount() == 0 )
 			return str + "}" + converterEngine.getNewline();
 			
-		str += converterEngine.getNewline();
+		str.append( converterEngine.getNewline() );
 		for ( int i = 0; i < instance.parameterCount(); i++ )
 		{
 			Parameter parameter = instance.parameter(i);
-			str += parameterWriter.toString( parameter, converterEngine, indent + "  " );
+			str.append( parameterWriter.toString( parameter, converterEngine, indent + "  " ) );
 		}
-		str += indent + "}" + converterEngine.getNewline();
-		return str;
+		str.append( indent + "}" + converterEngine.getNewline() );
+		return str.toString();
 	}
 
 
