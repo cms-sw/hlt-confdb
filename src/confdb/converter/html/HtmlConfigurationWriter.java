e9f884b8ab6717c2ee4fbbf6c60f745af34d4a09
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-10-24 10:09:02 +0000

    using StringBuffer

--- src/confdb/converter/html/HtmlConfigurationWriter.java
+++ src/confdb/converter/html/HtmlConfigurationWriter.java
@@ -28,118 +28,118 @@ public class HtmlConfigurationWriter implements IConfigurationWriter
 
 	public String toString( IConfiguration conf, WriteProcess writeProcess )
 	{
-		String str = "";
+		StringBuffer str = new StringBuffer();
 		if ( writeProcess == WriteProcess.YES )
-			str = "process <b>" + conf.processName() + "</b> = {" + converterEngine.getNewline();
+			str.append( "process <b>" + conf.processName() + "</b> = {" + converterEngine.getNewline() );
 
-		str += converterEngine.getNewline(); 
+		str.append( converterEngine.getNewline() ); 
 
 		if ( conf.psetCount() > 0 )
 		{
-			str += "<a name=\"psets\"></a>"; 
+			str.append( "<a name=\"psets\"></a>" ); 
 			IParameterWriter parameterWriter = converterEngine.getParameterWriter();
 			for ( int i = 0; i < conf.psetCount(); i++ )
 			{
 				Parameter pset = conf.pset(i);
-				str += parameterWriter.toString( pset, converterEngine, "  " );
+				str.append( parameterWriter.toString( pset, converterEngine, "  " ) );
 			}
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 		}
 
 		if ( conf.pathCount() > 0 )
 		{
-			str += "<a name=\"paths\"></a>";
+			str.append( "<a name=\"paths\"></a>" );
 			IPathWriter pathWriter = converterEngine.getPathWriter();
 			for ( int i = 0; i < conf.pathCount(); i++ )
 			{
 				Path path = conf.path(i);
-				str += wrapLine( pathWriter.toString(path, converterEngine, "  "),
-							     '&', 12 + path.name().length() );
+				str.append( wrapLine( pathWriter.toString(path, converterEngine, "  "),
+							     '&', 12 + path.name().length() ) );
 			}
 			if ( conf.sequenceCount() == 0 )
-				str += converterEngine.getNewline();
+				str.append( converterEngine.getNewline() ); 
 		}
 
 		
 		if ( conf.sequenceCount() > 0 )
 		{
-			str += "<a name=\"sequences\"></a>" + converterEngine.getNewline(); 
+			str.append( "<a name=\"sequences\"></a>" + converterEngine.getNewline() ); 
 			ISequenceWriter sequenceWriter = converterEngine.getSequenceWriter();
 			for ( int i = 0; i < conf.sequenceCount(); i++ )
 			{
 				Sequence sequence = conf.sequence(i);
-				str += wrapLine( sequenceWriter.toString(sequence, converterEngine ),
-								 '&', 16 + sequence.name().length() );
+				str.append( wrapLine( sequenceWriter.toString(sequence, converterEngine ),
+								 '&', 16 + sequence.name().length() ) );
 			}
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 		}
 
 		
 		if ( conf.moduleCount() > 0 )
 		{
-			str += "<a name=\"modules\"></a>"; 
+			str.append( "<a name=\"modules\"></a>" ); 
 			IModuleWriter moduleWriter = converterEngine.getModuleWriter();
 			for ( int i = 0; i < conf.moduleCount(); i++ )
 			{
 				ModuleInstance module = conf.module(i);
-				str += moduleWriter.toString( module );
+				str.append( moduleWriter.toString( module ) );
 			}
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 		}
 
 		if ( conf.edsourceCount() > 0 )
-			str += "<a name=\"ed_sources\"></a>"; 
+			str.append( "<a name=\"ed_sources\"></a>" ); 
 		IEDSourceWriter edsourceWriter = converterEngine.getEDSourceWriter();
 		for ( int i = 0; i < conf.edsourceCount(); i++ )
 		{
 			EDSourceInstance edsource = conf.edsource(i);
-			str += edsourceWriter.toString(edsource, converterEngine );
+			str.append( edsourceWriter.toString(edsource, converterEngine ) );
 		}
 		if ( conf.edsourceCount() == 0 )  // edsource may be overridden
-			str += edsourceWriter.toString( null, converterEngine );
+			str.append( edsourceWriter.toString( null, converterEngine ) );
 		if ( conf.edsourceCount() > 0 )
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 
 
 		if ( conf.essourceCount() > 0 )
 		{
-			str += "<a name=\"es_sources\"></a>"; 
+			str.append( "<a name=\"es_sources\"></a>" ); 
 			IESSourceWriter essourceWriter = converterEngine.getESSourceWriter();
 			for ( int i = 0; i < conf.essourceCount(); i++ )
 			{
 				ESSourceInstance essource = conf.essource(i);
-				str += essourceWriter.toString(essource, converterEngine);
+				str.append( essourceWriter.toString(essource, converterEngine) );
 			}
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 		}
 
 
 		if ( conf.esmoduleCount() > 0 )
 		{
-			str += "<a name=\"es_modules\"></a>"; 
+			str.append( "<a name=\"es_modules\"></a>" ); 
 			IESModuleWriter esmoduleWriter = converterEngine.getESModuleWriter();
 			for ( int i = 0; i < conf.esmoduleCount(); i++ )
 			{
 				ESModuleInstance esmodule = conf.esmodule(i);
-				str += esmoduleWriter.toString(esmodule,converterEngine);
+				str.append( esmoduleWriter.toString(esmodule,converterEngine) );
 			}
-			str += converterEngine.getNewline(); 
+			str.append( converterEngine.getNewline() ); 
 		}
 
 		if ( conf.serviceCount() > 0 )
 		{
-			str += "<a name=\"services\"></a>"; 
+			str.append( "<a name=\"services\"></a>" ); 
 			IServiceWriter serviceWriter = converterEngine.getServiceWriter();
 			for ( int i = 0; i < conf.serviceCount(); i++ )
 			{
 				ServiceInstance service = conf.service(i);
-				str += serviceWriter.toString( service, converterEngine );
+				str.append( serviceWriter.toString( service, converterEngine ) );
 			}
 		}
 
 		if ( writeProcess == WriteProcess.YES )
-			str += converterEngine.getConfigurationTrailer();
-		return str;
+			str.append( converterEngine.getConfigurationTrailer() );
+		return str.toString();
 	}
 
 	public void setConverterEngine(ConverterEngine converterEngine) {
