93b0ec9bfba6014d2756bd3664e438d047fb7dfb
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-11-07 16:07:42 +0000

    finalize(), deleteConverter() added

--- browser/src/browser/BrowserConverter.java
+++ browser/src/browser/BrowserConverter.java
@@ -1,22 +1,30 @@
 package browser;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import confdb.converter.ConverterBase;
 import confdb.converter.ConverterException;
 import confdb.converter.DbProperties;
 import confdb.data.IConfiguration;
+import confdb.db.ConfDB;
 import confdb.db.ConfDBSetups;
 
 public class BrowserConverter extends ConverterBase 
 {
 	static private HashMap<Integer,BrowserConverter> map = new HashMap<Integer,BrowserConverter>();
 
-	private BrowserConverter( String dbUrl, String dbUser, String dbPwrd ) throws ConverterException
+	private BrowserConverter( String dbType, String dbUrl, String dbUser, String dbPwrd ) throws ConverterException
 	{
-		super( "HTML", "oracle", dbUrl, dbUser, dbPwrd );	
+		super( "HTML", dbType, dbUrl, dbUser, dbPwrd );	
 	}
 		
+	public ConfDB getDatabase()
+	{
+		return super.getDatabase();
+	}
+	
 	public IConfiguration getConfiguration( int key )
 	{
 		return super.getConfiguration( key );
@@ -27,6 +35,14 @@ public class BrowserConverter extends ConverterBase
 		return getConverterEngine().convert( conf );
 	}
 
+	protected void finalize() throws Throwable
+	{
+		super.finalize();
+		ConfDB db = getDatabase();
+		if ( db != null )
+			db.disconnect();
+	}
+	
 	static public BrowserConverter getConverter( int dbIndex ) throws ConverterException 
 	{
 		ConfDBSetups dbs = new ConfDBSetups();
@@ -35,13 +51,24 @@ public class BrowserConverter extends ConverterBase
 		BrowserConverter converter = map.get( new Integer( dbIndex ) );
 		if ( converter == null )
 		{
-			converter = new BrowserConverter( dbProperties.getDbURL(), dbProperties.getDbUser(), "convertme!" );		
+			converter = new BrowserConverter( dbs.type( dbIndex ), dbProperties.getDbURL(), dbProperties.getDbUser(), "convertme!" );		
 			map.put( new Integer( dbIndex ), converter );
 		}
 		return converter;
 	}
 
-
+	static public void deleteConverter( BrowserConverter converter )
+	{
+		Set<Map.Entry<Integer,BrowserConverter>> entries = map.entrySet();
+		for ( Map.Entry<Integer,BrowserConverter> entry : entries )
+		{
+			if ( entry.getValue() == converter )
+			{
+				map.remove( entry.getKey() );
+				return;
+			}
+		}
+	}
 	
 	
 }
