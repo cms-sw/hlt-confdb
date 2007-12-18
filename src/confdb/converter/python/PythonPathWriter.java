c009f77a0c745701324aeb64cc7661686b8cd093
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2007-12-18 16:56:28 +0000

    first version generating python output

--- src/confdb/converter/python/PythonPathWriter.java
+++ src/confdb/converter/python/PythonPathWriter.java
@@ -8,32 +8,26 @@ public class PythonPathWriter implements IPathWriter
 {
 	public String toString( Path path, ConverterEngine converterEngine, String indent ) 
 	{
-		String str = indent;
-		if ( path.isEndPath() )
-			str += "endpath ";
-		else
-			str += "path ";
-		str += decorateName( path.name() ) + " = { ";
+		//if ( path.isEndPath() )
+		String str = indent + "'"+ path.name() +  "' : ( "; 
 		for ( int i = 0; i < path.entryCount(); i++  )
 		{
 			str += decorate( path.entry(i).name() );
 			if ( i + 1 < path.entryCount() )
-				str += " & ";
+				str += ", ";
 		}
-		str += " }" + converterEngine.getNewline();
+		str += " )";
 		return str;
 	}
-
-	
-	protected String decorate( String name )
-	{
-		return name;
-	}
-	
 	
 	protected String decorateName( String name )
 	{
 		return name;
 	}
-	
+
+	protected String decorate( String name )
+	{
+		return "'" + name + "'";
+	}
+
 }
