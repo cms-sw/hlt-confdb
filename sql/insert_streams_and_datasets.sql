4b716a3453cb3713009a53dbf14521c45f150744
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2008-03-27 18:39:29 +0000

    *** empty log message ***

---
+++ sql/insert_streams_and_datasets.sql
@@ -0,0 +1,23 @@
+--
+-- STREAM
+--
+INSERT INTO Streams(streamLabel) VALUES('A');
+INSERT INTO Streams(streamLabel) VALUES('LOW');
+INSERT INTO Streams(streamLabel) VALUES('MEDIUM');
+INSERT INTO Streams(streamLabel) VALUES('HIGH');
+INSERT INTO Streams(streamLabel) VALUES('EXRPESS');
+INSERT INTO Streams(streamLabel) VALUES('CALIB');
+
+
+--
+-- PRIMARY DATASETS
+--
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('Express');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('AlCa');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDAllEvents');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDBJet');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDElectron');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDJetMET');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDMuon');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDPhoton');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDTau');
