624ce3bc08547bb5a05be47375cb831bdff49210
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-03-31 15:06:02 +0000

    using cms.Service

--- src/confdb/converter/python/PythonServiceWriter.java
+++ src/confdb/converter/python/PythonServiceWriter.java
@@ -2,15 +2,54 @@ package confdb.converter.python;
 
 import confdb.converter.ConverterEngine;
 import confdb.converter.ConverterException;
+import confdb.converter.IParameterWriter;
 import confdb.converter.IServiceWriter;
+import confdb.data.Parameter;
 import confdb.data.ServiceInstance;
 
 
-public class PythonServiceWriter extends PythonInstanceWriter implements IServiceWriter 
+public class PythonServiceWriter implements IServiceWriter 
 {
-	public String toString( ServiceInstance service, ConverterEngine converterEngine, String indent ) throws ConverterException 
+	public String toString( ServiceInstance instance, ConverterEngine converterEngine, String indent ) throws ConverterException 
 	{
-		return toString( "service", service, converterEngine, indent);
+		if ( instance == null )
+			return "";
+
+		IParameterWriter parameterWriter = converterEngine.getParameterWriter();
+		
+		StringBuffer str = new StringBuffer( "process." + instance.name() + " = cms.Service( \""
+		    + instance.name() + "\"," );
+			
+		str.append( converterEngine.getNewline() );
+		for ( int i = 0; i < instance.parameterCount(); i++ )
+		{
+			Parameter parameter = instance.parameter(i);
+			String para = parameterWriter.toString( parameter, converterEngine, indent + "  " );
+			if ( para.length() > 0 )
+			{
+				if ( i < instance.parameterCount() - 1 )
+				{
+					if ( para.endsWith( "\n" ) )
+					{
+						str.append( para, 0, para.length() - 1 );
+						str.append( ",\n" );
+					}
+					else
+					{
+						str.append( para );
+						str.append( "," );
+					}
+				}
+				else
+					str.append( para );
+			}
+		}
+
+		str.append( ")\n" );
+
+		return str.toString();
 	}
+
+
 
 }
