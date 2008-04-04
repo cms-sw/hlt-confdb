303aaa699d27a6f2ab51666b949b9fc8641d7148
Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch>
2008-04-04 07:48:56 +0000

    *** empty log message ***

--- sql/insert_streams_and_datasets.sql
+++ sql/insert_streams_and_datasets.sql
@@ -6,7 +6,7 @@ INSERT INTO Streams(streamLabel) VALUES('LOW');
 INSERT INTO Streams(streamLabel) VALUES('MEDIUM');
 INSERT INTO Streams(streamLabel) VALUES('HIGH');
 INSERT INTO Streams(streamLabel) VALUES('EXRPESS');
-INSERT INTO Streams(streamLabel) VALUES('CALIB');
+INSERT INTO Streams(streamLabel) VALUES('ALCA');
 
 
 --
@@ -14,10 +14,23 @@ INSERT INTO Streams(streamLabel) VALUES('CALIB');
 --
 INSERT INTO PrimaryDatasets(datasetLabel) VALUES('Express');
 INSERT INTO PrimaryDatasets(datasetLabel) VALUES('AlCa');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDAllEvents');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDBJet');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDElectron');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDJetMET');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDMuon');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDPhoton');
-INSERT INTO PrimaryDatasets(datasetLabel) VALUES('PDTau');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('Minbias');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET20');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET30');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET50');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET80');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET110');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('JetET150');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('MuonPT5');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('MuonPT11');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('MuonOnia');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('LeptonEWK');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('MuonCosmicBON');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('MuonCosmicBOFF');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('TrackerCosmicBON');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('TrackerCosmicBOFF');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('HaloMuon');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('TrackerLaser');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('HCALNZS');
+INSERT INTO PrimaryDatasets(datasetLabel) VALUES('HCALIsoTrack');
+
