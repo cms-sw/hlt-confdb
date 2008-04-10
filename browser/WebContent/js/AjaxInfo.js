bb7ae2200e55fb1b10def3a25a104b616647d436
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-04-10 16:19:40 +0000

    first release

---
+++ browser/WebContent/js/AjaxInfo.js
@@ -0,0 +1,29 @@
+// Provide a default path to dwr.engine
+if (dwr == null) var dwr = {};
+if (dwr.engine == null) dwr.engine = {};
+if (DWREngine == null) var DWREngine = dwr.engine;
+
+if (AjaxInfo == null) var AjaxInfo = {};
+AjaxInfo._path = '/cms-project-confdb-hltdev/dwr';
+
+AjaxInfo.listDBs = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'listDBs', callback);
+}
+AjaxInfo.getAnchors = function(p0, p1, callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getAnchors', 'int:' + p0, 'int:' + p1, callback);
+}
+AjaxInfo.getRcmsDbInfo = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getRcmsDbInfo', callback);
+}
+AjaxInfo.getMaxMemory = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getMaxMemory', callback);
+}
+AjaxInfo.clearCache = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'clearCache', callback);
+}
+AjaxInfo.getFreeMemory = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getFreeMemory', callback);
+}
+AjaxInfo.getTotalMemory = function(callback) {
+  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getTotalMemory', callback);
+}
