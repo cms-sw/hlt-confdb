e7d7e8a9595c5f15eed056473d3cfd9e65ddfd53
cmsbuild <>
2008-04-21 12:03:27 +0000

    This commit was manufactured by cvs2git to create tag 'V00-22-08'.
    Sprout from master 2008-04-21 12:03:26 UTC Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch> 'tag V00-22-08: see ChangeLog.'
    Delete:
        src/confdb/converter/summary/json/JsonConfigurationWriter.java
        src/confdb/converter/summary/json/JsonEDSourceWriter.java
        src/confdb/converter/summary/json/JsonESModuleWriter.java
        src/confdb/converter/summary/json/JsonESSourceWriter.java
        src/confdb/converter/summary/json/JsonEmptyWriter.java
        src/confdb/converter/summary/json/JsonModuleWriter.java
        src/confdb/converter/summary/json/JsonParameterWriter.java
        src/confdb/converter/summary/json/JsonPathWriter.java
        src/confdb/converter/summary/json/JsonSequenceWriter.java
        src/confdb/converter/summary/json/JsonServiceWriter.java
        src/confdb/converter/summary/json/JsonWriter.java

--- src/confdb/converter/summary/json/JsonEDSourceWriter.java
+++
@@ -1,8 +0,0 @@
-package confdb.converter.summary.json;
-
-import confdb.converter.IEDSourceWriter;
-
-
-public class JsonEDSourceWriter extends JsonEmptyWriter implements IEDSourceWriter 
-{
-}
--- src/confdb/converter/summary/json/JsonESModuleWriter.java
+++
@@ -1,7 +0,0 @@
-package confdb.converter.summary.json;
-
-import confdb.converter.IESModuleWriter;
-
-public class JsonESModuleWriter extends JsonEmptyWriter implements IESModuleWriter 
-{
-}
