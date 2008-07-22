8d6c48bd7075a30463cda9b423aef3ad33ee212b
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-07-22 13:57:44 +0000

    no longer using ajax.AjaxInfo

--- browser/WebContent/js/AjaxInfo.js
+++ browser/WebContent/js/AjaxInfo.js
@@ -4,20 +4,20 @@ if (dwr.engine == null) dwr.engine = {};
 if (DWREngine == null) var DWREngine = dwr.engine;
 
 if (AjaxInfo == null) var AjaxInfo = {};
-AjaxInfo._path = '/cms-project-confdb-hltdev/dwr';
+AjaxInfo._path = '';
 
 AjaxInfo.listDBs = function(callback) {
-  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'listDBs', callback);
+  dwr.engine._execute(AjaxInfo._path, 'confdb.converter.BrowserConverter', 'listDBs', callback);
 }
 AjaxInfo.getAnchors = function(p0, p1, callback) {
-  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getAnchors', 'int:' + p0, 'int:' + p1, callback);
+  dwr.engine._execute(AjaxInfo._path, 'confdb.converter.BrowserConverter', 'getAnchors', 'int:' + p0, 'int:' + p1, callback);
 }
 AjaxInfo.getRcmsDbInfo = function(callback) {
-  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getRcmsDbInfo', callback);
+  dwr.engine._execute(AjaxInfo._path, 'AjaxInfo', 'getRcmsDbInfo', callback);
 }
 AjaxInfo.clearCache = function(callback) {
-  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'clearCache', callback);
+  dwr.engine._execute(AjaxInfo._path, 'confdb.converter.BrowserConverter', 'clearCache', callback);
 }
 AjaxInfo.getMemInfo = function(callback) {
-  dwr.engine._execute(AjaxInfo._path, 'ajax.AjaxInfo', 'getMemInfo', callback);
+  dwr.engine._execute(AjaxInfo._path, 'AjaxInfo', 'getMemInfo', callback);
 }
