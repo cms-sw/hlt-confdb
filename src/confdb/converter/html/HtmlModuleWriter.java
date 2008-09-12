ab9003fa22f9dbf95111b5196caae79ba017e7fc
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-09-12 13:25:29 +0000

    method appendType(..) added

--- src/confdb/converter/html/HtmlModuleWriter.java
+++ src/confdb/converter/html/HtmlModuleWriter.java
@@ -2,14 +2,30 @@ package confdb.converter.html;
 
 import confdb.converter.IModuleWriter;
 import confdb.converter.ascii.AsciiModuleWriter;
+import confdb.data.IConfiguration;
+import confdb.data.ModuleInstance;
 
 
 public class HtmlModuleWriter extends AsciiModuleWriter implements IModuleWriter 
 {
+	private String release = "";
 	
 	protected String decorate( String name )
 	{
 		return "<a name=\"" + name + "\"><b>" + name + "</b></a>";
 	}
 	
+	protected void appendType( StringBuffer str, ModuleInstance module )
+	{
+		String type = module.template().name();
+
+		IConfiguration iconfig = module.config();
+		if ( !release.equals( iconfig.releaseTag() ) )
+		{
+			release = iconfig.releaseTag();
+			System.out.println( "release = " + release );
+		}
+
+		str.append( "<a href=\"http://cmslxr.fnal.gov/lxr/ident?v=" + release + ";i=" + type + "\" target=\"_blank\">" + type + "</a>" );
+	}
 }
