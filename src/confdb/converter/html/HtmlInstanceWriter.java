0717468417fb2e06f1d69599be4c1521715d8165
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-09-12 13:22:20 +0000

    getName(), getTemplateName() added

--- src/confdb/converter/html/HtmlInstanceWriter.java
+++ src/confdb/converter/html/HtmlInstanceWriter.java
@@ -1,7 +1,25 @@
 package confdb.converter.html;
 
 import confdb.converter.ascii.AsciiInstanceWriter;
+import confdb.data.Instance;
 
 public class HtmlInstanceWriter extends AsciiInstanceWriter 
-{
+{	
+	@Override
+	protected String getName(Instance instance) 
+	{
+		return "<b>" + instance.name() + "</b>";
+	}
+
+	@Override
+	protected String getTemplateName( Instance instance ) 
+	{
+		String type = instance.template().name();
+
+		String release = instance.config().releaseTag();
+
+		return "<a href=\"http://cmslxr.fnal.gov/lxr/ident?v=" + release + ";i=" + type + "\" target=\"_blank\">" + type + "</a>";
+	}
+	
+	
 }
