a030997639512eb7502b8b6ba5c87d3f2396ae4e
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2008-11-14 15:28:03 +0000

    AjaxInfo added here

--- browser/WebContent/js/HLT.js
+++ browser/WebContent/js/HLT.js
@@ -75,3 +75,27 @@ HLTjs.setValue = function( ele, val )
   ele.innerHTML = val;
 }
 
+
+if (AjaxInfo == null) var AjaxInfo = {};
+AjaxInfo._path = '../json/AjaxInfo.jsp';
+
+AjaxInfo.listDBs = function(callback) {
+  HLTjs.json._execute(AjaxInfo._path, 'confdb.converter.BrowserConverter', 'listDBs', callback);
+}
+AjaxInfo.getAnchors = function(p0, p1, callback) {
+  HLTjs.json._execute(AjaxInfo._path, 'confdb.converter.BrowserConverter', 'getAnchors', 'int:' + p0, 'int:' + p1, callback);
+}
+AjaxInfo.getRcmsDbInfo = function(callback) {
+  HLTjs.json._execute(AjaxInfo._path, null, 'getRcmsDbInfo', callback);
+}
+AjaxInfo.clearCache = function(callback) {
+  HLTjs.json._execute(AjaxInfo._path, 'confdb.converter.ConfCache', 'clearCache', callback);
+}
+AjaxInfo.getMemInfo = function(callback) {
+  HLTjs.json._execute(AjaxInfo._path, null, 'getMemInfo', callback);
+}
+AjaxInfo.getTree = function( p0, callback) {
+  HLTjs.json._execute(AjaxInfo._path, null, 'getTree', 'string:' + p0, callback);
+}
+
+
