df35b37995c838455f2a4784568e0f7418a17369
Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch>
2009-02-25 16:11:56 +0000

    Fix for the list of components without cfi's.

--- parser/ConfdbPyconfigParser.py
+++ parser/ConfdbPyconfigParser.py
@@ -41,68 +41,73 @@ class ConfdbPyconfigParser:
         # Look at what cfi_py configs are available
         thedirectory = thedirectory.split('data/')[0] + 'python/'
         if(os.path.isdir(thedirectory)):
-           cfipyfiles = os.listdir(thedirectory)
-           for cfipyfile in cfipyfiles:
-               if(cfipyfile.endswith('_cfi.py')):
-                   thecomponent = cfipyfile.split('.py')[0]
-
-                   thebasecomponent = thecomponent.split('_cfi')[0]        
-
-                   # Construct the py-cfi to import
-                   thesubsystempackage = thedirectory.split('src/')[1].split('/data/')[0].lstrip().rstrip()
-                   thesubsystem = thesubsystempackage.split('/')[0]
-                   thepackage = thesubsystempackage.split('/')[1]
-                   importcommand = "import " + thesubsystem + "." + thepackage + "." + thecomponent
-
-                   if(self.verbose > 2):
-                       print "PSet = " + self.thenestedpset + ", " + self.thepset + ", " + self.theparameter
-
-                   if(self.verbose > 2):
-                       print 'Starting python session'
+            cfipyfiles = os.listdir(thedirectory)
+            for cfipyfile in cfipyfiles:
+                if(cfipyfile.endswith('_cfi.py')):
+                    thecomponent = cfipyfile.split('.py')[0]
+                    
+                    thebasecomponent = thecomponent.split('_cfi')[0]        
+                    
+                    # Construct the py-cfi to import
+                    thesubsystempackage = thedirectory.split('src/')[1].split('/data/')[0].lstrip().rstrip()
+                    thesubsystem = thesubsystempackage.split('/')[0]
+                    thepackage = thesubsystempackage.split('/')[1]
+                    importcommand = "import " + thesubsystem + "." + thepackage + "." + thecomponent
+                    
+                    if(self.verbose > 2):
+                        print "PSet = " + self.thenestedpset + ", " + self.thepset + ", " + self.theparameter
+                        
+                    if(self.verbose > 2):
+                        print 'Starting python session'
         
-                   try:
-                       if(self.verbose > 2):
-                           print "\t\t" + importcommand
+                    try:
+                        if(self.verbose > 2):
+                            print "\t\t" + importcommand
 
-                       exec importcommand
+                        exec importcommand
 
-                       # Now create a process and construct the command to extend it with the py-cfi
-                       process = cms.Process("MyProcess")
-                       theextend = "process.extend(" + thesubsystem + "." + thepackage + "." + thecomponent + ")"
-                       if(self.verbose > 2):
-                           print "\t\t" + theextend
-                       eval(theextend)
+                        # Now create a process and construct the command to extend it with the py-cfi
+                        process = cms.Process("MyProcess")
+                        theextend = "process.extend(" + thesubsystem + "." + thepackage + "." + thecomponent + ")"
+                        if(self.verbose > 2):
+                            print "\t\t" + theextend
+                        eval(theextend)
 
-                       self.foundcomponent = True
-                       
-                       # Now construct the command to query the variable value
-                       valvar = "process."
-                       valvar = valvar + thebasecomponent + "."
-                       if(self.thenestedpset != None and self.thenestedpset != ''):
-                           valvar = valvar + self.thenestedpset + "."
-                       if(self.thepset != None and self.thepset != ''):
-                           valvar = valvar + self.thepset + "."
-                       valvar = valvar + self.theparameter + "."
-                       valvar = valvar + "value()"
+                        # Now construct the command to query the variable value
+                        valvar = "process."
+                        valvar = valvar + thebasecomponent + "."
+                        valcomp = "process." + thebasecomponent
+                        if(self.thenestedpset != None and self.thenestedpset != ''):
+                            valvar = valvar + self.thenestedpset + "."
+                        if(self.thepset != None and self.thepset != ''):
+                            valvar = valvar + self.thepset + "."
+                        valvar = valvar + self.theparameter + "."
+                        valvar = valvar + "value()"
 
-                       if(self.verbose > 2):
-                           print "\t\t" + valvar
+                        if(self.verbose > 2):
+                            print "\t\t" + valcomp
+                        eval(valcomp)
+
+                        self.foundcomponent = True
+
+                        if(self.verbose > 2):
+                            print "\t\t" + valvar
             
-                       # OK, now get the default!
-                       thedefault =  eval(valvar)
+                        # OK, now get the default!
+                        thedefault =  eval(valvar)
 
-                       if(self.verbose > 2):
-                           print 'The default value of variable ' + self.theparameter + ' is:\n'        
-                           print thedefault
+                        if(self.verbose > 2):
+                            print 'The default value of variable ' + self.theparameter + ' is:\n'        
+                            print thedefault
 
-                       self.theparamdefault = thedefault
+                        self.theparamdefault = thedefault
 
-                       self.founddefault = True
+                        self.founddefault = True
                        
-                   except:
-                       if(self.verbose > 2):
-                           print 'Could not get python-config information - no variable default found'
-
+                    except:
+                        if(self.verbose > 2):
+                            print 'Could not get python-config information - no variable default found'
+                            
 
     def RetrievePythonConfigFoundComponent(self):
         return self.foundcomponent
