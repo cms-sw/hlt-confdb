d1ddfd32b750b3986f6dec2515bd4483cb95de60
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2011-04-01 10:35:52 +0000

    & -> +

--- src/confdb/converter/html/HtmlSequenceWriter.java
+++ src/confdb/converter/html/HtmlSequenceWriter.java
@@ -1,10 +1,27 @@
 package confdb.converter.html;
 
+import confdb.converter.ConverterEngine;
 import confdb.converter.ISequenceWriter;
 import confdb.converter.ascii.AsciiSequenceWriter;
+import confdb.data.Sequence;
 
-public class HtmlSequenceWriter extends AsciiSequenceWriter implements ISequenceWriter 
+public class HtmlSequenceWriter implements ISequenceWriter 
 {
+	public String toString( Sequence sequence, ConverterEngine converterEngine, String indent ) 
+	{
+		String str = indent + "sequence " + decorateName( sequence.name()) 
+			       + " = ";
+		for ( int i = 0; i < sequence.entryCount(); i++  )
+		{
+			str += decorate( sequence.entry(i).name() );
+			if ( i + 1 < sequence.entryCount() )
+				str += " + ";
+		}
+		str += converterEngine.getNewline();
+		return str;
+	}
+	
+
 	protected String decorate( String moduleName )
 	{
 		return "<a href=\"#" + moduleName + "\">" + moduleName + "</a>";
