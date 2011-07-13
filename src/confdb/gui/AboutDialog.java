afd7eb6fa29d7e6ead6f9778fd542e4c77a89756
jimeneze <sha1-690f9b4697725bd909f0892195f6c56b91e57474@cern.ch>
2011-07-13 13:50:57 +0000

    I forgot to put the Tag number in AboutDialog.java. (raul)

--- src/confdb/gui/AboutDialog.java
+++ src/confdb/gui/AboutDialog.java
@@ -51,7 +51,7 @@ public class AboutDialog extends JDialog
 	    "<b>https://twiki.cern.ch/twiki/bin/view/CMS/EvfConfDBGUI</b>.</p>";
 
 	jTextFieldApplication.setText("ConfDbGUI");
-	jTextFieldVersion.setText("V01-05-61");
+	jTextFieldVersion.setText("V01-05-62");
 
 	jEditorPaneAbout.setContentType("text/html");
 	jEditorPaneAbout.setText(txt);
