e992f60a5fdbf8dffad2599f45706df515a3413a
jimeneze <sha1-690f9b4697725bd909f0892195f6c56b91e57474@cern.ch>
2012-04-14 11:59:02 +0000

    tag V01-06-49: 	* Introduces load balancing URLs for the browser. 	* Add multiple hosts at confdb.properties using comma-separator.

--- src/confdb/converter/DbProperties.java
+++ src/confdb/converter/DbProperties.java
@@ -93,11 +93,37 @@ public class DbProperties
 	private void initURL()
 	{
 		dbURL = "jdbc:mysql://" + dbHost + ":3306/" + dbName;
-		if ( dbType.equals("oracle") )
-		    dbURL = "jdbc:oracle:thin:@//" + dbHost + ":10121/" + dbName;
-		
+		if ( dbType.equals("oracle") ) {
+			//dbURL = "jdbc:oracle:thin:@//" + dbHost + ":10121/" + dbName;
+			// Change to make load balancing:
+			dbURL = createOracleURL();
+		}
 	}
+	
+	// This method creates TNS descriptor according to Oracle Standards.
+	// This is to include loadBalancing when there are many hosts in confdb.properties.
+	// @see /confdb.db/confDB.java
+    public String createOracleURL() {
+		String url = "jdbc:oracle:thin:@(DESCRIPTION =";
+		String[] hosts = dbHost.split(",");
+		
+		if(hosts.length == 0) return "ERROR TNSNAME FORMAT";
+		
+		for(int i = 0; i < hosts.length; i++) {
+				url+= "(ADDRESS = (PROTOCOL = TCP)(HOST = "+hosts[i]+")(PORT = 10121))\n";
+		}
 
+	    url+="(ENABLE=BROKEN) ";
+	    url+="(LOAD_BALANCE = yes) ";
+	    url+="(CONNECT_DATA = ";
+	    url+="  (SERVER = DEDICATED) ";
+	    url+=" (SERVICE_NAME = "+dbName+") ";
+	    url+="   (FAILOVER_MODE = (TYPE = SELECT)(METHOD = BASIC)(RETRIES = 200)(DELAY = 15)) ";
+	    url+=") )";
+		
+		return url;
+    }
+    
 	static public DbProperties getDefaultDbProperties() throws IOException, ConverterException
 	{
 		try {
