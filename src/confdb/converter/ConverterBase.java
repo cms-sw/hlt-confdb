e8c46bd563800ebc1fef9f64054a55f5376ae3f5
Ulf Behrens <sha1-730dbf727bcb08dd1729f524bbee0127394bd810@cern.ch>
2012-04-26 08:31:20 +0000

    addPSetForStreams(), addPSetForDatasets(): PSets only added if not already existing

--- src/confdb/converter/ConverterBase.java
+++ src/confdb/converter/ConverterBase.java
@@ -85,45 +85,56 @@ public class ConverterBase
     /** add a pset to the passed configuration containing all streams */
     protected void addPSetForStreams(IConfiguration config)
     {
-	if (config.streamCount()==0) return;
-	
-	PSetParameter pset = new PSetParameter("streams","",true);
-	Iterator<Stream> itS = config.streamIterator();
-	while (itS.hasNext()) {
-	    Stream stream = itS.next();
-	    StringBuffer valueAsString = new StringBuffer();
-	    Iterator<PrimaryDataset> itD = stream.datasetIterator();
-	    while (itD.hasNext()) {
-		if (valueAsString.length()>0) valueAsString.append(",");
-		valueAsString.append(itD.next().name());
-	    }
-	    pset.addParameter(new VStringParameter(stream.name(),
+    	if ( config.streamCount() == 0 ) 
+    		return;
+
+    	if ( config.pset("streams") != null )
+    		return;
+    	
+    	PSetParameter pset = new PSetParameter("streams","",true);
+    	Iterator<Stream> itS = config.streamIterator();
+    	while (itS.hasNext()) {
+    		Stream stream = itS.next();
+    		StringBuffer valueAsString = new StringBuffer();
+    		Iterator<PrimaryDataset> itD = stream.datasetIterator();
+    		while (itD.hasNext()) {
+    			if ( valueAsString.length() > 0 ) 
+    				valueAsString.append(",");
+    			valueAsString.append(itD.next().name());
+    		}
+    		pset.addParameter(new VStringParameter(stream.name(),
 						   valueAsString.toString(),
 						   true));
-	}
-	config.insertPSet(pset);
+    	}
+    	config.insertPSet(pset);
     }
 
     /** add pset to the passed configuration containing all datasets */
     protected void addPSetForDatasets(IConfiguration config)
     {
-	if (config.datasetCount()==0) return;
+    	if ( config.datasetCount() == 0 ) 
+    		return;
 	
-	PSetParameter pset = new PSetParameter("datasets","",true);
-	Iterator<PrimaryDataset> itD = config.datasetIterator();
-	while (itD.hasNext()) {
-	    PrimaryDataset dataset = itD.next();
-	    StringBuffer valueAsString = new StringBuffer();
-	    Iterator<Path> itP = dataset.pathIterator();
-	    while (itP.hasNext()) {
-		if (valueAsString.length()>0) valueAsString.append(",");
-		valueAsString.append(itP.next().name());
-	    }
-	    pset.addParameter(new VStringParameter(dataset.name(),
+    	if ( config.pset("datasets") != null )
+    		return;
+    	
+    	PSetParameter pset = new PSetParameter("datasets","",true);
+    	Iterator<PrimaryDataset> itD = config.datasetIterator();
+    	while ( itD.hasNext() ) 
+    	{
+    		PrimaryDataset dataset = itD.next();
+    		StringBuffer valueAsString = new StringBuffer();
+    		Iterator<Path> itP = dataset.pathIterator();
+    		while (itP.hasNext()) {
+    			if ( valueAsString.length() > 0 ) 
+    				valueAsString.append(",");
+    			valueAsString.append(itP.next().name());
+    		}
+    		pset.addParameter(new VStringParameter(dataset.name(),
 						   valueAsString.toString(),
 						   true));
-	}
-	config.insertPSet(pset);
+    	}
+    	config.insertPSet(pset);
     }
     
 }
