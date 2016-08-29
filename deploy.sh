cf0af01fe2aa3a1df1781a087c1fd25c79c807d7
CMS ConfDB <cms.confdb@cern.ch>
2016-08-29 19:03:17 +0200

    V01-08-06
    deploy at http://confdb.web.cern.ch/confdb/v1/

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,17 @@
 
 # set the target directory
 BASE=$HOME/www/v1
+URL=http://confdb.web.cern.ch/confdb/v1/gui/
 
 # create the target directory 
 mkdir -p $BASE
 
 # deploy the GUI and the support files
 unzip -o -q lib/confdb.war confdb.version gui/* -d $BASE/
-sed -i 's#$$codebase#http://confdb.web.cern.ch/confdb/gui/#' $BASE/gui/start.jnlp
+sed -i "s#\$\$codebase#$URL#" $BASE/gui/start.jnlp
 
 # deploy all .jar files for interactive use
 mkdir -p $BASE/lib
 cp lib/*.jar ext/signed/*.jar $BASE/lib
 
-echo "ConfDB GUI version `cat $BASE/confdb.version | grep confdb.version | cut -d= -f2` successfully deployed"
+echo "ConfDB GUI version `cat $BASE/confdb.version | grep confdb.version | cut -d= -f2` successfully deployed at $URL"
