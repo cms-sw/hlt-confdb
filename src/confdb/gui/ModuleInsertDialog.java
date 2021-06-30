ae3c6c89b179a577dd4f4f9fca435708430aaed7
Sam Harper <sam.j.harper@gmail.com>
2021-06-30 09:06:40 +0200

    now updated it to handle existing modules

--- src/confdb/gui/ModuleInsertDialog.java
+++ src/confdb/gui/ModuleInsertDialog.java
@@ -36,12 +36,19 @@ public class ModuleInsertDialog extends JDialog {
 	private JComboBox jComboBoxModule = new javax.swing.JComboBox();
 	private JButton jButtonOK = new javax.swing.JButton();
 	private JButton jButtonCancel = new javax.swing.JButton();
+    private JRadioButton jButtonNew = new JRadioButton("new instance");
+    private JRadioButton jButtonExisting = new JRadioButton("existing instance");
+    private JRadioButton jButtonClone = new JRadioButton("clone");
+
+
+    private boolean existingModules = false;
 	
 	
 	/** standard constructor */
 	public ModuleInsertDialog(JFrame jFrame, JTree tree) {
 		super(jFrame, "Module Inserter",true);
 		this.tree = tree;
+        this.existingModules = existingModules;
 
 		
 		jComboBoxModule.addActionListener(new ActionListener() {
@@ -61,7 +68,31 @@ public class ModuleInsertDialog extends JDialog {
 				setVisible(false);
 			}
 		});
-		
+        
+        jButtonNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				newButtonActionPerformed(e);               				
+			}
+		});
+        jButtonNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				newButtonActionPerformed(e);               				
+			}
+		});
+        jButtonExisting.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				existingButtonActionPerformed(e);               
+			}
+		});
+        jButtonClone.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cloneButtonActionPerformed(e);               
+			}
+		});
+        
+
+
+
 		setContentPane(initComponents());
 		
 	}
@@ -76,6 +107,22 @@ public class ModuleInsertDialog extends JDialog {
         System.err.println("clicked: "+jComboBoxModule.getSelectedItem());
         System.err.println("action: "+e);
     }
+    private void newButtonActionPerformed(ActionEvent e) {
+        setComboBoxEntriesNewMod();
+        System.err.println("newButton: ");
+        System.err.println("action: "+e);
+    }
+    private void existingButtonActionPerformed(ActionEvent e) {
+        System.err.println("existingButton: ");
+        System.err.println("action: "+e);
+        setComboBoxEntriesExistingMod(true);
+    }
+    private void cloneButtonActionPerformed(ActionEvent e) {
+        System.err.println("cloneButton: ");
+        System.err.println("action: "+e);
+        setComboBoxEntriesExistingMod(true);
+    }
+    
 
 	private void jComboBoxModelActionPerformed(ActionEvent e) {
         System.err.println(jComboBoxModule.getSelectedItem());
@@ -88,15 +135,31 @@ public class ModuleInsertDialog extends JDialog {
         jPanel.add(jComboBoxModule);
         jPanel.add(jButtonOK);
         jPanel.add(jButtonCancel);
-
-        setComboBox();
-		jButtonOK.setText("OK");		
+        jPanel.add(jButtonNew);
+        jPanel.add(jButtonExisting);
+        jPanel.add(jButtonClone);
+        
+        
+        ButtonGroup addGroup  = new ButtonGroup();
+        addGroup.add(jButtonNew);
+        addGroup.add(jButtonExisting);
+        addGroup.add(jButtonClone);
+        
+        initComboBox();
+        setComboBoxEntriesExistingMod(true);
+        jButtonExisting.setEnabled(true);
+        jButtonOK.setText("OK");		
 		jButtonCancel.setText("Cancel");
 
 		return jPanel;
 	}
 
-    private void setComboBox() {     
+    private void initComboBox() {     
+        jComboBoxModule.setEditable(true);
+		jComboBoxModule.setBorder(BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+		AutoCompletion.enable(jComboBoxModule);
+    }
+    private void setComboBoxEntriesNewMod() {     
         ConfigurationTreeModel model = (ConfigurationTreeModel) tree.getModel();
         Configuration config = (Configuration) model.getRoot(); 
 		SoftwareRelease release = config.release();
@@ -109,9 +172,27 @@ public class ModuleInsertDialog extends JDialog {
 				continue;
             jComboBoxModule.addItem(t.name());
         }
-        jComboBoxModule.setEditable(true);
-		jComboBoxModule.setBorder(BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-		AutoCompletion.enable(jComboBoxModule);
     }
 
+    private void setComboBoxEntriesExistingMod(boolean prefixType) {     
+        ConfigurationTreeModel model = (ConfigurationTreeModel) tree.getModel();
+        Configuration config = (Configuration) model.getRoot(); 
+        SoftwareRelease release = config.release();
+		Iterator<ModuleInstance> it = config.moduleIterator();        
+            
+        jComboBoxModule.removeAllItems();
+		while (it.hasNext()) {
+			ModuleInstance t = it.next();
+            if(prefixType) {
+                if(t.template()!=null){
+                    jComboBoxModule.addItem(t.template().name()+":"+t.name());
+                }else{
+                    jComboBoxModule.addItem("NULL:"+t.name());
+                }
+            }else{
+                jComboBoxModule.addItem(t.name());  
+            }
+			
+        }
+    }
 }
