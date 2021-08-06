d911a9a3fa08e6b5d162b58b4384326351c4107b
Philipp Maximilian Brummer <philipp.maximilian.brummer@cern.ch>
2021-08-06 11:19:36 +0200

    fix class name of HtmlSwitchProducerWriter to match expected spelling
    Change class name prefix from HTML to Html as expected by the ConverterFactory.
    
    References CMSHLT-2161

--- src/confdb/converter/html/HtmlSwitchProducerWriter.java
+++ src/confdb/converter/html/HtmlSwitchProducerWriter.java
@@ -4,7 +4,7 @@ import confdb.converter.ConverterEngine;
 import confdb.converter.ISwitchProducerWriter;
 import confdb.data.SwitchProducer;
 
-public class HTMLSwitchProducerWriter implements ISwitchProducerWriter {
+public class HtmlSwitchProducerWriter implements ISwitchProducerWriter {
 	public String toString(SwitchProducer switchProducer, ConverterEngine converterEngine, String indent) {
 		String str = indent + "switchProducer " + decorateName(switchProducer.name()) + " = ";
 		for (int i = 0; i < switchProducer.entryCount(); i++) {
