e0b4218102d981f8e26a862ca570e9e25bea91b0
Sam Harper <sam.j.harper@gmail.com>
2021-11-05 14:31:14 +0100

    adding hash to version file and fixing bug adding hash to the version number

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@
 #we get the branch from 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 VERSION=`git rev-parse --abbrev-ref HEAD | sed s/confdb//g`
+GIT_HASH=`git rev-parse HEAD`
 VALID_VERSIONS=("v2" "v3" "v3-beta" "v3-test")
 PROD_VERSIONS=("v2" "v3")
+
 if [[ ! " ${VALID_VERSIONS[*]} " =~ " ${VERSION} " ]]; then
     echo "branch $BRANCH gives version $VERSION which is not a valid version, which are:"
     echo "   ${VALID_VERSIONS[@]}"
@@ -14,7 +16,7 @@ fi
 
 if [[ ! " ${PROD_VERSIONS[*]} " =~ " ${VERSION} " ]]; then
     echo "non production version $VERSION detected, appending git hash to version number"
-    sed -i -E 's/(^confdb.version=V)([0-9\-]+)/\1\2-31f04d99d48a011b1d7e161fba1f5f2212c2b1db/g' src/conf/confdb.version 
+    sed -i -E 's/(^confdb.version=V)([0-9\-]+)/\1\2-'$GIT_HASH'/g' src/conf/confdb.version 
 fi
 
 BASE=$HOME/www/$VERSION
@@ -43,6 +45,7 @@ cp -a lib/*.jar ext/signed/*.jar $BASE/gui/
 
 # deploy the .jnlp and support files
 cp -a src/conf/confdb.version $BASE/
+echo "confdb.githash=$GIT_HASH" >> $BASE/confdb.version
 cp -a javaws/WebContent/index.html javaws/WebContent/start.jnlp $BASE/gui/
 
 sed -i "s#\$\$codebase#$URL#" $BASE/gui/start.jnlp
