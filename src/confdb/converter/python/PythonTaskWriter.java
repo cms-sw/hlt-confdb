bf099527c447d5ef8792633e44fe8fa5a4f6b149
Sam Harper <sam.j.harper@gmail.com>
2022-05-19 18:20:31 +0200

    making Tasks conditional in 12_4_0_pre4+

--- src/confdb/converter/python/PythonTaskWriter.java
+++ src/confdb/converter/python/PythonTaskWriter.java
@@ -6,10 +6,20 @@ import confdb.converter.ConverterEngine;
 import confdb.converter.ITaskWriter;
 import confdb.data.Reference;
 import confdb.data.Task;
+import confdb.data.ReleaseVersionInfo;
 
 public class PythonTaskWriter implements ITaskWriter {
 	public String toString(Task task, ConverterEngine converterEngine, String object) {
-		String str = object + task.name() + " = cms.Task( ";
+		
+		ReleaseVersionInfo relInfo = new ReleaseVersionInfo(task.config().releaseTag());
+		String taskType = null;
+		if(relInfo.geq(12,4,0,4)){
+			taskType = new String("cms.ConditionalTask");
+		}else{
+			taskType = new String("cms.Task");
+		}
+
+		String str = object + task.name() + " = "+taskType+"( ";
 		if (task.entryCount() > 0) {
 			String sep = " , ";
 			Iterator<Reference> list = task.entryIterator();
