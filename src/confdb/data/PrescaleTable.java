ea2ee4d58b59e69d04d422707e9544d4e129f335
Sam Harper <sam.j.harper@gmail.com>
2025-02-16 16:59:53 +0100

    making the ps table location only avaliable in one place

--- src/confdb/data/PrescaleTable.java
+++ src/confdb/data/PrescaleTable.java
@@ -37,7 +37,7 @@ public class PrescaleTable
 	public static final String PSTBLINFO_PSET_NAME = "PrescaleTableInfo";
 
 	// menu location we write the prescales to
-	public static final String PSTBL_CONFB_LOCATION = "/users/sharper/2025/test1/prescales/v1";
+	public static final String PSTBL_CONFDB_LOCATION = "/users/sharper/2025/test1/prescales/v1";
 
     //
     // construction
